9eaefca35c5a64b4b00956794b2fbf17d0ad5656
hxlfare <g.hxflare@gmail.com>
2026-02-11 20:48:33 +0200

    cat

--- coreutils/cat.c
+++ coreutils/cat.c
@@ -1,3 +1,34 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void cprint(char *string) {
+    write(1, string, strlen(string));
+}
+int main(int argc, char **argv) {
+    char *fullstr = NULL;
+    char ch;
+    FILE *fptr = fopen(argv[1], "r");
+    if (!fptr) {
+        cprint("shi... failed to open file");
+        return 1;
+    }
+    int index = 0;
+    while ((ch = fgetc(fptr)) != EOF) {
+        fullstr = realloc(fullstr, index + 2);
+        if (!fullstr) {
+            cprint("dementia");
+            fclose(fptr);
+            return 1;
+        }
+        fullstr[index++] = ch;
+    }
+    if (fullstr)
+        fullstr[index] = '\0';
+    if (fullstr)
+        cprint(fullstr);
+    free(fullstr);
+    fclose(fptr);
+    return 0;
+}
