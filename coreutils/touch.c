44c4ea55d3ad51f616ad6dd934846205b66e4a5d
hxlfare <g.hxflare@gmail.com>
2026-02-14 18:52:37 +0200

    working touch

---
+++ coreutils/touch.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <utime.h>
+void cprint(char *string)
+{
+    write(1, string, sizeof(string));
+}
+int touch(char *filename) {
+    int fd=open(filename, O_WRONLY | O_CREAT,0644);
+    if (fd==-1){
+        cprint("shi.... unable to open it.\n");
+    }
+    close(fd);
+    if (utime(filename, NULL) == -1) {
+        cprint("shi... unable to do time shit\n");
+        return -1;
+    }
+    return 0;
+}
+int main(int argc,char **argv){
+    for (int i=1;i<argc;i++){
+        if (touch(argv[i])!=0){
+            cprint("shi.. unable to open da file\n");
+        }
+    }
+}
